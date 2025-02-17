cb45c673df9f384b7304cb0ed37259e482e0791c
kagistan <eeshvarankagistan@gmail.com>
2025-02-17 14:01:17 +0100

    change content of result-template with index.js
    Signed-off-by: kagistan <eeshvarankagistan@gmail.com>

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 // Paths to files
 const recipePath = path.join(__dirname, 'recipe.json');
+const templatePath = path.join(__dirname, 'result-template.html');
+const outputPath = path.join(__dirname, 'result-template.html');
 
 // Read the recipe JSON file
 fs.readFile(recipePath, 'utf8', (err, data) => {
@@ -13,5 +15,28 @@ fs.readFile(recipePath, 'utf8', (err, data) => {
 
   const recipe = JSON.parse(data);
 
-  console.log('Recipe:', recipe);
+  // Build HTML content
+  let recipeHTML = `
+    <h1>TEST</h1>
+  `;
+
+  // Read the HTML template
+  fs.readFile(templatePath, 'utf8', (err, template) => {
+    if (err) {
+      console.error('Error reading result-template.html:', err);
+      return;
+    }
+
+    // Replace the placeholder with the generated HTML
+    const finalHTML = template.replace('REPLACEME', recipeHTML);
+
+    // Write the result to result.html
+    fs.writeFile(outputPath, finalHTML, (err) => {
+      if (err) {
+        console.error('Error writing result.html:', err);
+        return;
+      }
+      console.log('HTML recipe generated successfully!');
+    });
+  });
 });
